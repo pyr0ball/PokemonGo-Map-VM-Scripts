cc386bb7dc74b3fb41be39e44ac5ae6e3b118f8f
pyr0ball <pyroballpcs@gmail.com>
2016-08-26 21:27:53 -0700

    Updated update script with proper git behavior, updated submodules to latest

--- update-map.sh
+++ update-map.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-mv launch-server.sh launch-server.$(date +"%Y%m%d%H%M").old
+git stash
 git pull origin master
+git stash pop
 cd ~/PokemonGo-Map
 
 #check current installation's status
@@ -8,10 +9,12 @@ git status
 
 #look for conflicts before pull
 ### will create more stuff here later ###
-
-git pull
+git stash
+git pull origin develop
+git stash pop
 
 sudo -H pip install -r requirements.txt --upgrade
 sudo npm install
 sudo npm run build
-rm ~/.trash/*
+
+
