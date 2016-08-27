beb7ba2b69406e1c9ed012468724f1c0e9b4a949
pyr0ball <pyroballpcs@gmail.com>
2016-08-27 15:03:14 -0700

    added new version tracking and update system

--- update-map.sh
+++ update-map.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
-git stash
-git pull origin master
-git stash pop
-cd PokemonGo-Map
+#PokemonGo-Map-VM Update Script v0.2.3
 
-#check current installation's status
-git status
-
-#look for conflicts before pull
-### will create more stuff here later ###
-git stash
-git checkout develop
-git pull
-git stash pop
-
-sudo -H pip install -r requirements.txt --upgrade
-sudo npm install
-sudo npm run build
-
-
+## Check current version against master on gitHub. Run update on VM scripts if version mismatch
+git checkout .version
+ver=$(cat .version)
+if [ "$ver" != "v0.2.3" ] ; then
+	git stash
+	git pull origin master
+	git stash pop
+	sh ./update-map.sh
+else
+	cd PokemonGo-Map
+	#git pull map files, merge end-user changes
+	git stash
+	git checkout develop
+	git pull
+	git stash pop
+	
+	#run install steps on map
+	sudo -H pip install -r requirements.txt --upgrade
+	sudo npm install
+	sudo npm run build
+	
+	#git pull ptc-gen-files
+	cd ../ptc-acc-gen
+	git stash
+	git checkout master
+	git pull
+	git stash pop
+fi
